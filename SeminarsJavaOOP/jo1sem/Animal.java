1ae1d10c3b70d197b1a6b48f67adb66515e39639
“Alexandr <kuzmin_89@mail.ru>
2023-12-04 00:50:11 +0500

    prodoljenie1

--- SeminarsJavaOOP/jo1sem/Animal.java
+++ SeminarsJavaOOP/jo1sem/Animal.java
@@ -1,8 +1,26 @@
 import java.time.LocalDate;
 
 public class Animal {
-    String name;
-    String illnes;
-    String owerName;
-    LocalDate birthDay;
+    private String name;
+    public String illnes;
+    private String owerName;
+    private LocalDate birthDay;
+
+    public Animal(String name, String illnes, String owerName, LocalDate birthDay) {
+        this.name = name;
+        this.illnes = illnes;
+        this.owerName = owerName;
+        this.birthDay = birthDay;
+    }
+
+    public Animal() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- SeminarsJavaOOP/jo1sem/Main.java
+++ SeminarsJavaOOP/jo1sem/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Animal cat = new Animal();
+        //cat.setName("Marquis");
+        System.out.println(cat.getName());
+        Animal dog = new Animal("Barbos", "chumka", "petr",
+                LocalDate.of(2009,05,24));
+        System.out.printf(dog.getName());
     }
 }
