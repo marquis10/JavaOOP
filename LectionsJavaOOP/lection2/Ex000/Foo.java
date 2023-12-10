006332e9bb9d7e395d7698e9cad4c12eedb9330f
“Alexandr <kuzmin_89@mail.ru>
2023-12-10 11:57:26 +0500

    hw2

---
+++ LectionsJavaOOP/lection2/Ex000/Foo.java
@@ -0,0 +1,16 @@
+package JavaOOP.lection2.Ex000;
+
+public class Foo {
+    public Integer value;
+
+    public static Integer count;
+
+    public Foo(){
+
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
---
+++ LectionsJavaOOP/lection2/Ex000/Program.java
@@ -0,0 +1,18 @@
+package JavaOOP.lection2.Ex000;
+
+public class Program {
+    public static void main(String[] args) {
+        Foo f1 = new Foo();
+        f1.value = 123;
+        System.out.println(f1.value);
+
+        Foo f2 = new Foo();
+        f2.value = 222;
+        System.out.println(f2.value);
+
+        Foo f3 = new Foo();
+        f3.value = 333;
+        System.out.println(f3.value);
+
+    }
+}
